e840f1539b0250091f60180e8785e523fd0e5c4d
znkalena <elze66@gmail.com>
2023-06-26 22:12:59 +0300

    add split

--- js/script.js
+++ js/script.js
@@ -2,29 +2,34 @@ const menuBtn =document.querySelector('.user__btn-menu');
 const closeBtn =document.querySelector('.user__btn-close');
 const navigation =document.querySelector('.navigation');   
     
-
-
-menuBtn.addEventListener('click',() =>{            
-        navigation.classList.add('navigation-open');
-        closeBtn.classList.remove('user__btn-close');
-        closeBtn.classList.add('btn-open');
-        menuBtn.classList.add('user__btn-close');     
-});
-
-closeBtn.addEventListener('click',()=>{
-        navigation.classList.remove('navigation-open');        
-        menuBtn.classList.remove('user__btn-close');
-        closeBtn.classList.remove('btn-open');
-        closeBtn.classList.add('user__btn-close');
-});
-
-window.addEventListener('keydown',(evt) => {
-    if(evt.target.key ==='Esc' || navigation.classList.contains('navigation-open')){
-        navigation.classList.remove('navigation-open');        
-        menuBtn.classList.remove('user__btn-close');
-        closeBtn.classList.remove('btn-open');
-        closeBtn.classList.add('user__btn-close');
+document.addEventListener('DOMContentLoaded', function() {
+    if (navigation){
+        menuBtn.addEventListener('click',() =>{            
+            navigation.classList.add('navigation-open');
+            closeBtn.classList.remove('user__btn-close');
+            closeBtn.classList.add('btn-open');
+            menuBtn.classList.add('user__btn-close');     
+    });
+    
+    closeBtn.addEventListener('click',()=>{
+            navigation.classList.remove('navigation-open');        
+            menuBtn.classList.remove('user__btn-close');
+            closeBtn.classList.remove('btn-open');
+            closeBtn.classList.add('user__btn-close');
+    });
+    
+    window.addEventListener('keydown',(evt) => {
+        if(evt.target.key ==='Esc' || navigation.classList.contains('navigation-open')){
+            navigation.classList.remove('navigation-open');        
+            menuBtn.classList.remove('user__btn-close');
+            closeBtn.classList.remove('btn-open');
+            closeBtn.classList.add('user__btn-close');
+        }
+    })    
+    } else {
+        menuBtn.click();
     }
-})
+});
+
 
 
